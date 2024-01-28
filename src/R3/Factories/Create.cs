c48a5a5ebc003febfe9292f9b4794476099e6126
TORISOUP <tori.birdstrike@gmail.com>
2024-01-29 02:11:21 +0900

    Add async Create

--- src/R3/Factories/Create.cs
+++ src/R3/Factories/Create.cs
@@ -11,6 +11,16 @@ public static partial class Observable
     {
         return new AnonymousObservable<T, TState>(state, subscribe);
     }
+
+    public static Observable<T> Create<T>(Func<Observer<T>, CancellationToken, ValueTask> subscribe)
+    {
+        return new AsyncAnonymousObservable<T>(subscribe);
+    }
+
+    public static Observable<T> Create<T, TState>(TState state, Func<Observer<T>, TState, CancellationToken, ValueTask> subscribe)
+    {
+        return new AsyncAnonymousObservable<T, TState>(state, subscribe);
+    }
 }
 
 internal sealed class AnonymousObservable<T>(Func<Observer<T>, IDisposable> subscribe) : Observable<T>
@@ -21,10 +31,33 @@ internal sealed class AnonymousObservable<T>(Func<Observer<T>, IDisposable> subs
     }
 }
 
-internal sealed class AnonymousObservable<T, TState>(TState state, Func<Observer<T>, TState, IDisposable> subscribe) : Observable<T>
+internal sealed class AnonymousObservable<T, TState>(TState state, Func<Observer<T>, TState, IDisposable> subscribe)
+    : Observable<T>
 {
     protected override IDisposable SubscribeCore(Observer<T> observer)
     {
         return subscribe(observer, state);
+    }
+}
+
+internal sealed class AsyncAnonymousObservable<T>(Func<Observer<T>, CancellationToken, ValueTask> subscribe)
+    : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        var cancellationDisposable = new CancellationDisposable();
+        subscribe(observer, cancellationDisposable.Token);
+        return cancellationDisposable;
+    }
+}
+
+internal sealed class AsyncAnonymousObservable<T, TState>(TState state, Func<Observer<T>, TState, CancellationToken, ValueTask> subscribe)
+    : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        var cancellationDisposable = new CancellationDisposable();
+        subscribe(observer, state, cancellationDisposable.Token);
+        return cancellationDisposable;
     }
 }
